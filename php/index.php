82a62b7cb7e7bce7db943492fe8c163852b44344
omer <omer.cohen@kadabrainc.com>
2024-07-26 13:16:19 +0300

    new version

--- php/index.php
+++ php/index.php
@@ -9,8 +9,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("<div style='color: red; font-size: 24px; text-align: center; margin-top: 20%;'>
+            <strong>Oh no!</strong> Connection failed: " . $conn->connect_error . "
+         </div>");
 }
 
-echo "Connected successfully to the database.";
-?>
+echo "<div style='color: green; font-size: 24px; text-align: center; margin-top: 20%;'>
+        <strong>Woohoo!</strong> Connected successfully to the database.
+        <br><br>
+        <img src='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif' alt='Funny GIF' style='width: 300px; height: auto;'>
+      </div>";
+?>% 
